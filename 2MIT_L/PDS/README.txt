6c51624079bb096b49f2a31545919831810b4b54
Filip Kocica <xkocic01@stud.fit.vutbr.cz>
2021-02-22 20:03:23 +0100

    lv

--- 2MIT_L/PDS/README.txt
+++ 2MIT_L/PDS/README.txt
@@ -1,3 +1,10 @@
 http://blog.catchpoint.com/2017/05/12/dissecting-tls-using-wireshark/
 
 https://engineering.salesforce.com/tls-fingerprinting-with-ja3-and-ja3s-247362855967
+
+https://tools.ietf.org/html/draft-davidben-tls-grease-01  (odstraneni grease)
+
+https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml (odstraneni 21 - padding)
+
+jejich clanek - odstraneni renegotiation 65281
+              - cely ten popis jak to delali + JA3+JA3S increased uniqueness - jsem pouzil taky
--- 2MIT_L/PDS/src/extractTlsFingerprints.sh
+++ 2MIT_L/PDS/src/extractTlsFingerprints.sh
@@ -4,9 +4,9 @@
 # 02/21/2021
 #
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "Invalid arguments. Usage: extractTlsFingerprints.sh <path_to_data>"
+    echo "Invalid arguments. Usage: extractTlsFingerprints.sh <path_to_data> <path_to_whitelists>"
     exit 1
 fi
 
@@ -24,13 +24,33 @@ function computeFingerprint()
     c07=$(echo "$1" | awk -F';' '{printf "%s", $7}'  | tr -d '"') # Handshake ciphersuite
     c08=$(echo "$1" | awk -F';' '{printf "%s", $8}'  | tr -d '"') # Handshake extensions
     c09=$(echo "$1" | awk -F';' '{printf "%s", $9}'  | tr -d '"') # Handshake SNI
-    c10=$(echo "$1" | awk -F';' '{printf "%s", $10}' | tr -d '"') # Handshake supported EC
+    c10=$(echo "$1" | awk -F';' '{printf "%s", $10}' | tr -d '"') # Handshake supported groups (former EC)
     c11=$(echo "$1" | awk -F';' '{printf "%s", $11}' | tr -d '"') # Handshake EC type
+
+    declare -A grease=(
+      [2570]=1
+      [6682]=1
+      [10794]=1
+      [14906]=1
+      [19018]=1
+      [23130]=1
+      [27242]=1
+      [31354]=1
+      [35466]=1
+      [39578]=1
+      [43690]=1
+      [47802]=1
+      [51914]=1
+      [56026]=1
+      [60138]=1
+      [64250]=1
+      [65281]=1
+    )
 
     a=""
     for i in $(echo $c07 | sed "s/,/ /g")
     do
-        a="$a$i-"
+        [[ -n "${grease[$i]}" ]] || a="$a$i-"
     done
 
     if [ -n "$a" ]; then
@@ -40,7 +60,7 @@ function computeFingerprint()
     b=""
     for i in $(echo $c08 | sed "s/,/ /g")
     do
-        b="$b$i-"
+        [[ -n "${grease[$i]}" ]] || [[ "$i" = "21" ]] || b="$b$i-"
     done
 
     if [ -n "$b" ]; then
@@ -49,7 +69,7 @@ function computeFingerprint()
 
     for i in $(echo $c10 | sed "s/,/ /g")
     do
-        c="$c$(($i))-"
+        [[ -n "${grease[$(($i))]}" ]] || c="$c$(($i))-"
     done
 
     if [ -n "$c" ]; then
@@ -61,7 +81,7 @@ function computeFingerprint()
         md5ja3=`echo -n $ja3 | md5sum | awk '{ print $1 }'`
         echo "$md5ja3"
     else
-        ja3s="$(($c06)),$c07,$b"
+        ja3s="$(($c06)),$a,$b"
         md5ja3s=`echo -n $ja3s | md5sum | awk '{ print $1 }'`
         echo "$md5ja3s"
     fi
@@ -95,30 +115,54 @@ function lookupServerResponse()
 function extractTlsFingerprints()
 {
     srcFile=$1
+    whitelistsDir=$2
 
     declare -i i=0
+    declare -a whitelistArr
+
+    whitelist=${srcFile##*/}
+    whitelist=$(echo "$whitelist" | cut -f 1 -d '.')
+    whitelist="$whitelistsDir/$whitelist.txt"
+
+    while read -r keyword
+    do
+        whitelistArr+=($keyword)
+    done < $whitelist
+
     while read -r line
     do
         i=$(($i+1))
 
         if [ $(echo "$line" | awk -F';' '{printf "%s", $5}'  | tr -d '"') = "1" ]; then
-            clientHello=$line
-            serverHello=$(lookupServerResponse $srcFile $clientHello $i)
-
-            md5ja3=$(computeFingerprint $clientHello)
-            md5ja3s=$(computeFingerprint $serverHello)
-
             sni=$(echo "$line" | awk -F';' '{printf "%s", $9}'  | tr -d '"')
 
-            echo "$md5ja3;$md5ja3s;$sni;${srcFile##*/}"
+            pass=false
+            for whitelistSni in "${whitelistArr[@]}"
+            do
+                if [[ $sni == *"$whitelistSni"* ]]; then
+                    pass=true
+                    break
+                fi
+            done
+
+            if [ "$pass" = true ]; then
+                clientHello=$line
+                serverHello=$(lookupServerResponse $srcFile $clientHello $i)
+
+                md5ja3=$(computeFingerprint $clientHello)
+                md5ja3s=$(computeFingerprint $serverHello)
+
+                echo "$md5ja3;$md5ja3s;$sni;${srcFile##*/}"
+            fi
         fi
     done <<< $(tail -n +2 "$srcFile") # Skip header
 }
 
 sourceDir=$1
+whitelistsDir=$2
 
 echo "JA3;JA3S;SNI;File"
 
 for filename in $sourceDir/*.csv; do
-    extractTlsFingerprints $filename
+    extractTlsFingerprints $filename $whitelistsDir
 done
